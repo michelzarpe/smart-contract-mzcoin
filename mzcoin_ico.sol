8081bb078aab4ce17fdbcf20cbcc2ba7d4edd02e
michelzarpe <michelzarpe@gmail.com>
2021-06-08 19:49:19 -0300

    adicionando variaveis de contrato para controle da moeda

---
+++ mzcoin_ico.sol
@@ -0,0 +1,15 @@
+//ico mzcoin
+//versao
+pragma solidity ^0.4.11
+
+contract mzcoin_ico {
+    //numero maximo de mzcoin que serão introduzidas no ico
+    uint public max_mzcoin = 1000000;
+    //taxa cotacao de mzcoin para dollar
+    uint public usd_to_mzcoin = 1000;
+    //total de mzcoin que foram comprados por investidores
+    uint public total_mzcoin_bought = 0; 
+    
+    
+    
+}

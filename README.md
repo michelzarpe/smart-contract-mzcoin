aa6a41dc5a5295aed1bc17d9ebbe0842a11a4d4e
michelzarpe <michelzarpe@gmail.com>
2021-06-12 17:07:58 -0300

    funções para retornar o valor investido em mzcoins e dolares

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ Instalar | User:
 2. MyEtherWallet "carteira baseada em ethereum executar index.html"
 3. Remix Solidity: Ethereum IDE (questão de compatibilidade de versão) https://remix.ethereum.org/#appVersion=0.7.7&&version=soljson-v0.4.11+commit.68ef5810.js&optimize=false
 
+Documentação do solidity: 
+https://docs.soliditylang.org/en/latest/index.html#getting-started
+
 
 
--- mzcoin_ico.sol
+++ mzcoin_ico.sol
@@ -21,4 +21,17 @@ contract mzcoin_ico {
         require (usd_invested * usd_to_mzcoin + total_mzcoin_bought <= max_mzcoin);
         _;//funcao só sera aplicada onde a condição for verdadeira
     }    
+	
+	//retorna o valor do investimento em mzcoins
+	function equity_in_mzcoins(address investor)external constant returns(unit){
+		return equity_mzcoin[investor];
+	}
+	
+	//retorna o valor do investimento em dolares
+	function equity_in_usd(address investor)external constant returns(unit){
+		return equity_usd[investor];
+	}
+
+
+	
 }
